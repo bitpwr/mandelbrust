62499624fbd53d952cc24b205cd1acae8379933a
PM Holtmo <pm@unknown.com>
2019-12-31 12:04:21 +0100

    Showing basic window using sdl2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+extern crate sdl2;
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
+pub fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let window = video_subsystem
+        .window("MandelbRust", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window
+        .into_canvas()
+        .present_vsync()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    println!("Using SDL_Renderer \"{}\"", canvas.info().name);
+
+    let mut event_pump = sdl_context.event_pump()?;
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+    
+        canvas.set_draw_color(Color::RGB(170, 190, 230));
+        canvas.clear();
+        canvas.present();
+
+        ::std::thread::sleep(std::time::Duration::from_millis(20));
+    }
+
+    Ok(())
 }
