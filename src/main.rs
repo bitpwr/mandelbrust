9b81b46a7dd153d1d7034b4c5452a7e643da66e9
PM Holtmo <pm@unknown.com>
2020-01-05 22:51:02 +0100

    Transform handles all transformations

--- src/main.rs
+++ src/main.rs
@@ -12,36 +12,64 @@ use std::time::SystemTime;
 
 const MAX_ITER: u32 = 200;
 
-#[derive(Copy, Clone)]
-struct Mapping {
+struct Transform {
     x: f64,
     y: f64,
     scale: f64,
+    window_size: (u32, u32),
 }
 
-impl Mapping {
-    fn reset(&mut self, window_size: (u32, u32)) {
-        self.scale = window_size.0 as f64 * 0.28;
-        self.x = window_size.0 as f64 * 0.7;
-        self.y = window_size.1 as f64 * 0.5;
+impl Transform {
+    fn new(window_size: (u32, u32)) -> Self {
+        Transform {
+            scale: window_size.0 as f64 * 0.28,
+            x: window_size.0 as f64 * 0.7,
+            y: window_size.1 as f64 * 0.5,
+            window_size,
+        }
     }
 
-    fn zoom(&self, window_size: (u32, u32)) -> f64 {
-        self.scale / (window_size.0 as f64 * 0.28)
+    fn point_to_complex(&self, p: &Point) -> Complex<f64> {
+        self.pos_to_complex(p.x, p.y)
+    }
+
+    fn pos_to_complex(&self, x: i32, y: i32) -> Complex<f64> {
+        Complex::new(
+            (x as f64 - self.x) / self.scale,
+            (y as f64 - self.y) / self.scale,
+        )
+    }
+
+    fn _complex_to_point(&self, z: Complex<f64>) -> Point {
+        Point::new(
+            (z.re * self.scale + self.x) as i32,
+            (z.im * self.scale + self.y) as i32,
+        )
+    }
+
+    fn zoom(&mut self, factor: f64) {
+        let z_center = self.pos_to_complex(
+            (self.window_size.0 / 2) as i32,
+            (self.window_size.1 / 2) as i32,
+        );
+        self.scale *= factor;
+        self.x = self.window_size.0 as f64 / 2.0 - z_center.re * self.scale;
+        self.y = self.window_size.1 as f64 / 2.0 - z_center.im * self.scale;
+
+        println!("Zoom: {}", self.zoom_factor());
+    }
+
+    fn zoom_factor(&self) -> f64 {
+        self.scale / (self.window_size.0 as f64 * 0.28)
+    }
+
+    fn center_at(&mut self, z: &Complex<f64>) {
+        self.x = self.window_size.0 as f64 / 2.0 - z.re * self.scale;
+        self.y = self.window_size.1 as f64 / 2.0 - z.im * self.scale;
     }
 }
 
-// fn to_pix(z: &Complex<f64>) -> Point {
-//     Point::new((z.re * 200.0 + 500.0) as i32, (z.im * 200.0 + 300.0) as i32)
-// }
-
-fn to_complex(map: Mapping, p: Point) -> Complex<f64> {
-    Complex::new(
-        (p.x as f64 - map.x) / map.scale,
-        (p.y as f64 - map.y) / map.scale,
-    )
-}
-
+// TODO: make slice, no vec
 fn colors() -> Vec<Color> {
     let mut c: Vec<Color> = Vec::with_capacity(MAX_ITER as usize);
 
@@ -63,9 +91,9 @@ fn color(n: u32) -> Color {
     }
 }
 
-// check if z is definitely within the Mandelbort set
-// according to wikipedia
-fn in_set(z: Complex<f64>) -> bool {
+/// Checks if z is definitely within the Mandelbort set
+/// according to wikipedia
+fn in_set(z: &Complex<f64>) -> bool {
     let p = ((z.re - 0.25).powi(2) + z.im.powi(2)).sqrt();
 
     if z.re <= (p - 2.0 * p.powi(2) + 0.25) {
@@ -77,11 +105,11 @@ fn in_set(z: Complex<f64>) -> bool {
     false
 }
 
-fn mandel(c: Complex<f64>) -> u32 {
+fn mandel(c: &Complex<f64>) -> u32 {
     let f = |z| z * z + c;
     let mut iter = 0;
 
-    if in_set(c) {
+    if in_set(&c) {
         iter = MAX_ITER - 1;
     } else {
         let mut next = Complex::new(0.0, 0.0);
@@ -95,7 +123,10 @@ fn mandel(c: Complex<f64>) -> u32 {
     iter
 }
 
-fn draw(map: Mapping, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>) {
+fn draw(
+    transform: &Transform,
+    canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
+) -> Result<(), String> {
     let start = SystemTime::now();
 
     let window_size = canvas.output_size().unwrap();
@@ -104,26 +135,16 @@ fn draw(map: Mapping, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>) {
     for x in 0..window_size.0 {
         for y in 0..window_size.1 {
             let p = Point::new(x as i32, y as i32);
-            let z = to_complex(map, p);
-            let n = mandel(z);
+            let z = transform.point_to_complex(&p);
+            let n = mandel(&z);
             canvas.set_draw_color(c[n as usize]);
-            let _e = canvas.draw_point(p);
+            canvas.draw_point(p)?;
         }
     }
 
     println!("Time: {:?}", start.elapsed().unwrap());
-}
 
-fn zoom(map: &mut Mapping, window_size: (u32, u32), factor: f64) {
-    let z_center = to_complex(
-        *map,
-        Point::new((window_size.0 / 2) as i32, (window_size.1 / 2) as i32),
-    );
-    map.scale *= factor;
-    map.x = window_size.0 as f64 / 2.0 - z_center.re * map.scale;
-    map.y = window_size.1 as f64 / 2.0 - z_center.im * map.scale;
-
-    println!("Zoom: {}", map.zoom(window_size));
+    Ok(())
 }
 
 pub fn main() -> Result<(), String> {
@@ -147,12 +168,7 @@ pub fn main() -> Result<(), String> {
     let window_size = canvas.output_size()?;
     println!("Windows size {:?}", window_size);
 
-    let mut map = Mapping {
-        x: 500.0,
-        y: 300.0,
-        scale: 200.0,
-    };
-    map.reset(window_size);
+    let mut transform = Transform::new(window_size);
 
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
@@ -173,21 +189,21 @@ pub fn main() -> Result<(), String> {
                     keycode: Some(Keycode::Plus),
                     ..
                 } => {
-                    zoom(&mut map, window_size, 1.5);
+                    transform.zoom(1.5);
                     update = true;
                 }
                 Event::KeyDown {
                     keycode: Some(Keycode::Minus),
                     ..
                 } => {
-                    zoom(&mut map, window_size, 0.66);
+                    transform.zoom(2.0 / 3.0);
                     update = true;
                 }
                 Event::KeyDown {
                     keycode: Some(Keycode::Space),
                     ..
                 } => {
-                    map.reset(window_size);
+                    transform = Transform::new(window_size);
                     update = true;
                 }
                 Event::MouseButtonDown {
@@ -196,10 +212,7 @@ pub fn main() -> Result<(), String> {
                     mouse_btn: MouseButton::Left,
                     ..
                 } => {
-                    let z = to_complex(map, Point::new(x, y));
-                    println!("New center {:?}", z);
-                    map.x = window_size.0 as f64 / 2.0 - z.re * map.scale;
-                    map.y = window_size.1 as f64 / 2.0 - z.im * map.scale;
+                    transform.center_at(&transform.pos_to_complex(x, y));
                     update = true;
                 }
                 Event::MouseButtonDown {
@@ -208,15 +221,15 @@ pub fn main() -> Result<(), String> {
                     mouse_btn: MouseButton::Right,
                     ..
                 } => {
-                    let z = to_complex(map, Point::new(x, y));
-                    println!("{:?} iter: {}", z, mandel(z));
+                    let z = transform.pos_to_complex(x, y);
+                    println!("{:?} iter: {}", z, mandel(&z));
                 }
                 _ => {}
             }
         }
 
         if update {
-            draw(map, &mut canvas);
+            draw(&transform, &mut canvas)?;
             update = false;
             canvas.present();
         }
@@ -233,12 +246,24 @@ mod tests {
 
     #[test]
     fn test_transforms() {
-        // let p = Point::new(100,100);
-        // let z = to_complex(p);
-        // assert_eq!(p, to_pix(&z));
+        let transform = Transform::new((200, 300));
+        let p = Point::new(100, 100);
+        let z = transform.point_to_complex(&p);
+        assert_eq!(p, transform._complex_to_point(z));
+    }
 
-        // let mut z2 = Complex::new(0.0, 0.0);
-        // to_complex2(&p, &mut z2);
-        // assert_eq!(z, z2);
+    #[test]
+    fn test_zoom() {
+        let mut transform = Transform::new((200, 300));
+        assert_eq!(transform.zoom_factor(), 1.0);
+
+        transform.zoom(2.0);
+        assert_eq!(transform.zoom_factor(), 2.0);
+
+        transform.zoom(5.0);
+        assert_eq!(transform.zoom_factor(), 10.0);
+
+        transform.zoom(0.5);
+        assert_eq!(transform.zoom_factor(), 5.0);
     }
 }
